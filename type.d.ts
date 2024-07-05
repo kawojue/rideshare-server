2a43fbd2ca638b88e5803662d8d8fa6249443b90
Raheem Kawojue <kawojue08@gmail.com>
2024-07-05 17:05:48 +0100

    chore: init

---
+++ type.d.ts
@@ -0,0 +1,34 @@
+interface CloudinaryModuleOptions {
+    cloudName: string
+    apiKey: string
+    apiSecret: string
+}
+
+interface FileDest {
+    folder: string
+    resource_type: 'image' | 'video'
+}
+
+interface IGenOTP {
+    totp: string
+    totp_expiry: Date
+}
+
+type Roles = 'ADMIN' | 'DRIVER' | 'PASSENGER'
+type Status = 'ACTIVE' | 'SUSPENDED'
+
+interface ExpressUser extends Express.User {
+    sub: string
+    role: Role
+    status?: Status
+}
+
+interface IRequest extends Request {
+    user: ExpressUser
+}
+
+interface JwtPayload {
+    sub: string
+    role: Roles
+    status?: Status
+}
